8facdb465c414e0bb64cb00e7a1d2ea7330ce6af
Aldona Tomzik <a.tomzik@gmail.com>
2020-08-28 16:13:19 +0200

    Added fixed menu and menu background color change on scroll

--- src/app.js
+++ src/app.js
@@ -1,2 +1,3 @@
 import "bootstrap";
 import "./scss/main.scss";
+import "./js/changeColorOnScroll";
---
+++ src/js/changeColorOnScroll.js
@@ -0,0 +1,11 @@
+const navbar = document.querySelector(".main-nav");
+const header = document.querySelector(".header");
+const changeNavbarColorOnScroll = function (event) {
+  if (window.pageYOffset > header.offsetHeight) {
+    navbar.classList.add("primary");
+  } else {
+    navbar.classList.remove("primary");
+  }
+};
+
+window.addEventListener("scroll", changeNavbarColorOnScroll);
